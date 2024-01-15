42e5fe0aac20f7cbca32c3cbeabcd6cd6d14e7c0
dufresax <axeldufresnegonsolin@gmail.com>
2024-01-15 14:46:45 +0100

    ff

--- src/UtilitairePaireChaineEntier.java
+++ src/UtilitairePaireChaineEntier.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class UtilitairePaireChaineEntier {
 
 
     public static int indicePourChaine(ArrayList<PaireChaineEntier> listePaires, String chaine) {
-        return 0;
-
+        int i = 0;
+        while (i < listePaires.size() && !Objects.equals(listePaires.get(i).getChaine(), chaine)) {
+            i++;
+        }
+        if (i != listePaires.size()) {
+            return i;
+        } else {
+            return -1;
+        }
     }
+
 
     public static int entierPourChaine(ArrayList<PaireChaineEntier> listePaires, String chaine) {
         int i = 0;
-        while (i < listePaires.size() && listePaires.get(i).getChaine().compareTo(chaine) !=0) {
+        while (i < listePaires.size() && listePaires.get(i).getChaine().compareTo(chaine) != 0) {
             i++;
         }
 
-        if (i < listePaires.size()-1) {
+        if (i < listePaires.size() - 1) {
             return listePaires.get(i).getEntier();
         } else {
             return 0;
@@ -37,10 +46,6 @@ public class UtilitairePaireChaineEntier {
     public static float moyenne(ArrayList<PaireChaineEntier> listePaires) {
         return 0;
     }
-
-
-
-
 
 
 }
